4cd4b6eb7e8f1caf9e4ded7a7896653a5ea534c4
Rehan Ayub <rehanayub20@gmail.com>
2020-05-16 20:38:32 -0400

    Added pipes and chaining

--- app6.js
+++ app6.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
+const zlib = require('zlib');
+const gzip = zlib.createGzip();
 const readStream = fs.createReadStream('./textfiles/randText.txt', 'utf8');
-const writeStream = fs.createWriteStream('./textfiles/write2.txt');
+const writeStream = fs.createWriteStream('./textfiles/write2.txt.gz');
 
 //console.log(readStream);
 
-readStream.on('data', chunk => {
-    //console.log(chunk);
-    writeStream.write(chunk);
-    //can write data in before it is fully loaded
-});
+// readStream.on('data', chunk => {
+//     //console.log(chunk);
+//     writeStream.write(chunk);
+//     //can write data in before it is fully loaded
+// });
+//Above is equivalent to below
+//readStream.pipe(writeStream);
+readStream.pipe(gzip).pipe(writeStream);
